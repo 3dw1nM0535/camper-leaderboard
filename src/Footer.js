7b157565ddba19da27f8c316ad6293bb013fa5fb
Edwin Moses <workockmoses@gmail.com>
2017-06-21 23:50:52 +0300

    footer component

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component{
+  render() {
+    return(
+      <div className="container text-center">
+        <p className="lead">BY Edwin Moses Lomolo</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
