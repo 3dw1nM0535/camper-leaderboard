4fa9a81c76ada15c0355d18c2e6689da873da7a5
Edwin Moses <workockmoses@gmail.com>
2017-06-21 23:50:07 +0300

    footer component render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Home from './components/Home';
+import Footer from './Footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Home />
+        <Footer />
       </div>
     );
   }
