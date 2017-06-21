6fb6ca04084d57b4ed715ec9b7ebba83accb4ec1
Edwin Moses <workockmoses@gmail.com>
2017-06-21 23:50:33 +0300

    Home render

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+class Home extends React.Component {
+  render() {
+    return(
+      <div className='container'>
+        <div className='jumbotron text-center'>
+          <h3 className="display-4">freeCodeCamp Leaderboard</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
