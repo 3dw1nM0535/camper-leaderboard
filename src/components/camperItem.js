5e328edeb4d95fbe6601f3a9a8b1a74cce9d9ff2
Edwin Moses <workockmoses@gmail.com>
2017-06-22 20:33:27 +0300

    count this.props

--- src/components/camperItem.js
+++ src/components/camperItem.js
@@ -6,7 +6,7 @@ class CamperItem extends React.Component{
     return(
       <tbody>
         <tr>
-          <th scope="row">{this.props.index}</th>
+          <th scope="row">{this.props.count}</th>
           <td><img className="rounded-circle" src={this.props.camper.img} alt="Camper.avatar" width="40rem" height="40rem" /> {this.props.camper.username}</td>
           <td>{this.props.camper.recent}</td>
           <td>{this.props.camper.alltime}</td>
