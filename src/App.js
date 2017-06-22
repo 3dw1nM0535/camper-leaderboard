fe6de6914785f6fc5d110e8841b43e005a52da40
Edwin Moses <workockmoses@gmail.com>
2017-06-22 09:16:20 +0300

    API call in lifecycle method

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import Home from './components/Home';
 import Footer from './Footer';
 import './App.css';
@@ -11,8 +12,24 @@ class App extends Component {
     }
   }
 
-  componentWillMount() {
+  getCampers() {
+    $.ajax({
+      url: "https://fcctop100.herokuapp.com/api/fccusers/top/recent",
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({campers: data}, function() {
+          console.log(this.state);
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(err);
+      }
+    });
+  }
 
+  componentWillMount() {
+    this.getCampers();
   }
 
   render() {
