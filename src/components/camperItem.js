3354c6c9b4064553f53ffb39f2ce0da1ba41f71f
Edwin Moses <workockmoses@gmail.com>
2017-06-22 10:46:50 +0300

    camper details component

---
+++ src/components/camperItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class CamperItem extends React.Component{
+  render() {
+    return(
+      <tbody>
+        <tr>
+          <th scope="row">{this.props.index}</th>
+          <td><img className="rounded-circle" src={this.props.camper.img} alt="Camper.avatar" width="30rem" height="30rem" /> {this.props.camper.username}</td>
+          <td>{this.props.camper.recent}</td>
+          <td>{this.props.camper.alltime}</td>
+        </tr>
+      </tbody>
+    );
+  }
+}
+
+export default CamperItem;
