bfb82109d12454adb3d5d655ffe6efd59c192a25
Edwin Moses <workockmoses@gmail.com>
2017-06-22 20:39:06 +0300

    Enabled striped table

--- src/components/camperItem.js
+++ src/components/camperItem.js
@@ -4,14 +4,12 @@ import '../App.css';
 class CamperItem extends React.Component{
   render() {
     return(
-      <tbody>
-        <tr>
-          <th scope="row">{this.props.count}</th>
-          <td><img className="rounded-circle" src={this.props.camper.img} alt="Camper.avatar" width="40rem" height="40rem" /> {this.props.camper.username}</td>
-          <td>{this.props.camper.recent}</td>
-          <td>{this.props.camper.alltime}</td>
-        </tr>
-      </tbody>
+      <tr>
+        <th scope="row">{this.props.count}</th>
+        <td><img className="rounded-circle" src={this.props.camper.img} alt="Camper.avatar" width="40rem" height="40rem" /> {this.props.camper.username}</td>
+        <td>{this.props.camper.recent}</td>
+        <td>{this.props.camper.alltime}</td>
+      </tr>
     );
   }
 }
